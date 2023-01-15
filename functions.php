06286394a09dfb75e1e06dd8a5812449db126c5e
atiikmahmud <atikmahmud.cse@gmail.com>
2023-01-15 16:57:45 +0600

    css file and js file link

--- functions.php
+++ functions.php
@@ -5,3 +5,20 @@
 
 // Theme Title
 add_theme_support('title-tag');
+
+//Theme CSS and JQuery File calling
+function super_css_js_file_calling()
+{
+    wp_enqueue_style('super-style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.2.3', 'all');
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('custom');
+
+    //jQuery
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.2.3', 'true');
+    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
+}
+add_action('wp_enqueue_scripts', 'super_css_js_file_calling');
